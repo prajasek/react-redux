4591612b9f821129c7b2ae5741b4bad3ab8d7f66
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2022-06-02 09:24:16 -0400

    add comments for increment action creator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import reducer, {try1} from './features/counter/counterSlice';
+import reducer, {try1, /* increment */} from './features/counter/counterSlice';
 import { useDispatch, useSelector } from 'react-redux';
+ 
 
 function App() {
   console.log("Rendering...")
 
   let dispatch = useDispatch();
   let state = useSelector((state) => state);
-  //dispatch({type: 'counter/increment'})
   console.log("State is ", state);
 
   return (
  <div>
   <button onClick={() => 
-    dispatch({type:"counter/increment"})}
+    dispatch({type:"counter/increment"})}   // same as 'dispatch(increment())'
+                                            // 'increment' action-creator from counterSlice.actions
    > Increment </button>
  </div>
   );
